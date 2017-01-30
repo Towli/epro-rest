11935533c26501c2a0cb5b9fd03cc267da55d32d
Alex Towli <alextowli@hotmail.co.uk>
2017-01-30 22:58:27 +0000

    add before-save callback for patient schema

--- models/patient.js
+++ models/patient.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
-// create a schema for patient model
+/* create a schema for patient model */
 var patientSchema = new Schema({
 	first_name: String,
 	last_name: String,
@@ -10,6 +10,15 @@ var patientSchema = new Schema({
 	surveyed: Boolean,
 	created_at: Date,
 	updated_at: Date
+});
+
+/* Before-save callback */
+patientSchema.pre('save', function(next) {
+	var currentDate = new Date();
+	if (!this.created_at)
+		this.created_at = currentDate;
+	this.updated_at = currentDate;
+	next();
 });
 
 var Patient = mongoose.model('Patient', patientSchema);
