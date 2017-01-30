d663247a7f363952c661ebc9cc9b6780a510cf0d
Alex Towli <alextowli@hotmail.co.uk>
2017-01-30 23:33:19 +0000

    add ability to persist patients

--- routes/patients.js
+++ routes/patients.js
@@ -1,13 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var Patient = require('../models/patient');
 
 /* GET /patients/ (index page) */
 router.get('/patients', function(req, res, next) {
-  res.render('patients/index');
+	/* Find all patients */
+	Patient.find({}, function(err, patients) {
+		if (err) throw err;
+		res.render('patients/index', {patients : patients});
+	});
 });
 
 /* GET /patients/new (new patient page) */
 router.get('/patients/new/', function(req, res, next) {
+	res.render('patients/new');
+});
+
+router.post('/patients/new', function(req, res, next) {
+	
+	/* Create a new patient from req params */
+	var patient = new Patient({
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		dob: req.body.dob,
+		gender: req.body.gender,
+		surveyed: false	// TODO functionality
+	});
+
+	/* Call the built-in save method to persist to db */
+	patient.save(function(err) {
+		if (err) throw err;
+		console.log('Patient saved successfully.');
+		console.log(patient);
+	});
+
 	res.render('patients/new');
 });
 
