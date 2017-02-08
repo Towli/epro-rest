9df42fde1ade0894e7f8cbce1e6f2d72d024b5a7
Alex Towli <alextowli@hotmail.co.uk>
2017-02-08 16:08:40 +0000

    add route for generating patient-specific survey

--- routes/surveys.js
+++ routes/surveys.js
@@ -1,24 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var Patient = require('../models/patient');
+var Procedure = require('../models/procedure');
 
 /* GET /surveys/ (index page) */
 router.get('/surveys', function(req, res, next) {
-		res.render('surveys/index');
+	res.render('surveys/index');
+});
+
+/* GET /surveys/dynamic (dynamically generated survey page) */
+router.get('/surveys/dynamic', function(req, res, next) {
+	Patient.findOne({ first_name: 'Alex'}, function(err, patient) {
+		if (err) return err;
+		res.render('surveys/dynamic', {patient:patient});
+	});
 });
 
 /* GET /surveys/example1 (example survey page) */
 router.get('/surveys/example1', function(req, res, next) {
-		res.render('surveys/example');
+	res.render('surveys/example');
 });
 
 /* GET /surveys/example2 (example survey page) */
 router.get('/surveys/example2', function(req, res, next) {
-		res.render('surveys/example2');
+	res.render('surveys/example2');
 });
 
 /* GET /surveys/example3 (example survey page) */
 router.get('/surveys/example3', function(req, res, next) {
-		res.render('surveys/example3');
+	res.render('surveys/example3');
 });
 
 module.exports = router;
