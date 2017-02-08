b5ee92627460557f5e0299d824869618221a94f1
Alex Towli <alextowli@hotmail.co.uk>
2017-02-08 15:07:07 +0000

    add routes and example surveys

---
+++ public/javascripts/surveys1.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	Survey.defaultBootstrapCss.navigationButton = "btn btn-primary";
+	Survey.Survey.cssType = "bootstrap";
+	var survey = new Survey.Model( { questions: [
+	     {name:"name", type:"text", title: "Please enter your name:", placeHolder:"Jon Snow", isRequired: true},
+	     {name:"birthdate", type:"text", inputType:"date", title: "Your birthdate:", isRequired: true},
+	     {name:"color", type:"text", inputType:"color", title: "Your favorite color:"},
+	     {name:"email", type:"text", inputType:"email", title: "Your e-mail:", placeHolder:"jon.snow@nightwatch.org", isRequired: true, validators: [{type:"email"}]}
+	]});
+
+	function sendDataToServer(survey) {
+		var resultAsString = JSON.stringify(survey.data);
+  		$("#results").append("<h3>JSON Results</h3>");
+  		$("#results").append(resultAsString);
+	}
+
+	$("#surveyElement").Survey({model:survey, onComplete:sendDataToServer});
+
+});
--- routes/surveys.js
+++ routes/surveys.js
@@ -6,29 +6,19 @@ router.get('/surveys', function(req, res, next) {
 		res.render('surveys/index');
 });
 
-/* GET /surveys/example (example survey page) */
-router.get('/surveys/example', function(req, res, next) {
-		
-		var surveyJSON = { 
-			title: "Tell us, what technologies do you use?", 
-			pages: [
-			  { name:"page1", questions: [ 
-			      { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Do you use any front-end framework like Bootstrap?" },
-			      { type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visible: false }
-			   ]},
-			  { name: "page2", questions: [
-			    { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
-			    { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visible: false } ] },
-			  { name: "page3",questions: [
-			    { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
-			 ],
-			 triggers: [
-			  { type: "visible", operator: "equal", value: "Yes", name: "frameworkUsing", questions: ["framework"]},
-			  { type: "visible", operator: "equal", value: "Yes", name: "mvvmUsing", questions: ["mvvm"]}
-			 ]
-		}
-
+/* GET /surveys/example1 (example survey page) */
+router.get('/surveys/example1', function(req, res, next) {
 		res.render('surveys/example');
+});
+
+/* GET /surveys/example2 (example survey page) */
+router.get('/surveys/example2', function(req, res, next) {
+		res.render('surveys/example2');
+});
+
+/* GET /surveys/example3 (example survey page) */
+router.get('/surveys/example3', function(req, res, next) {
+		res.render('surveys/example3');
 });
 
 module.exports = router;
