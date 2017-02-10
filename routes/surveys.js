115fed497f7017d2801eb37ae2d1d90f1ef074f4
Alex Towli <alextowli@hotmail.co.uk>
2017-02-10 20:49:02 +0000

    remove example survey 3 and route to generate survey page

--- routes/surveys.js
+++ routes/surveys.js
@@ -37,6 +37,13 @@ router.get('/surveys/dynamic', function(req, res, next) {
 	});
 });
 
+router.get('/surveys/new', function(req, res, next) {
+	Procedure.find({}, function(err, procedures) {
+		if (err) throw err;
+		res.render('surveys/new', { procedures : procedures });
+	});
+});
+
 /* GET /surveys/example1 (example survey page) */
 router.get('/surveys/example1', function(req, res, next) {
 	res.render('surveys/example');
@@ -45,11 +52,6 @@ router.get('/surveys/example1', function(req, res, next) {
 /* GET /surveys/example2 (example survey page) */
 router.get('/surveys/example2', function(req, res, next) {
 	res.render('surveys/example2');
-});
-
-/* GET /surveys/example3 (example survey page) */
-router.get('/surveys/example3', function(req, res, next) {
-	res.render('surveys/example3');
 });
 
 module.exports = router;
