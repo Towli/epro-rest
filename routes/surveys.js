096b932ee640a35f2b692636a70ca6e9362674d6
Alex Towli <alextowli@hotmail.co.uk>
2017-02-11 01:46:54 +0000

    feature: choose a procedure to generate survey for

--- routes/surveys.js
+++ routes/surveys.js
@@ -37,10 +37,37 @@ router.get('/surveys/dynamic', function(req, res, next) {
 	});
 });
 
+/* GET /surveys/new (survey creation page) */
 router.get('/surveys/new', function(req, res, next) {
 	Procedure.find({}, function(err, procedures) {
 		if (err) throw err;
 		res.render('surveys/new', { procedures : procedures });
+	});
+});
+
+/* POST /surveys/new (on survey creation) */
+router.post('/surveys/new', function(req, res, next) {
+	/* Get procedure from req params */
+	Procedure.findById(req.body.procedure)
+	.populate('procedure').exec(function(err, procedure) {
+		if (err) err;
+		/* Create a new survey from req params */
+		var question1 = "Question 1 for " + procedure.name;
+		var question2 = "Question 2 for " + procedure.name; 
+		var question3 = "Question 3 for " + procedure.name;
+		var question4 = "Question 4 for " + procedure.name;
+
+		var survey = new Survey({
+			patient: null,
+			questionSet: {
+				question1: question1,
+				question2: question2,
+				question3: question3,
+				question4: question4 
+			},
+			completed: false
+		});
+		res.render('surveys/show', { survey : survey });
 	});
 });
 
@@ -52,6 +79,11 @@ router.get('/surveys/example1', function(req, res, next) {
 /* GET /surveys/example2 (example survey page) */
 router.get('/surveys/example2', function(req, res, next) {
 	res.render('surveys/example2');
+});
+
+/* GET /surveys/:id (show survey) */
+router.get('/surveys/:id', function(req, res, next) {
+	res.render('surveys/show'); //todo
 });
 
 module.exports = router;
