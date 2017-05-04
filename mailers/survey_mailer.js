5af672499d513aa6465eeb846c7a742d196bd23e
Alex Towli <alextowli@hotmail.co.uk>
2017-05-04 21:41:27 +0100

    move mailer into OO based file

---
+++ mailers/survey_mailer.js
@@ -0,0 +1,51 @@
+var nodemailer = require('nodemailer');
+
+function Mailer() {
+  this.options = {
+    from: "",
+    to: "",
+    subject: "",
+    html: ""
+  }
+  this.initialiseTransporter();
+}
+
+Mailer.prototype.initialiseTransporter = function() {
+  this.transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+          user: "alextowli@gmail.com",
+          pass: "Alex7towli1114"
+      }
+  });
+}
+
+Mailer.prototype.sendMail = function() {
+  this.transporter.sendMail(mailOptions, function(error, info) {
+    if (error) throw err;
+    console.log('Message %s sent: %s', info.messageId, info.response);
+    transporter.close();
+  });
+}
+
+Mailer.prototype.setSender = function(sender) {
+  this.options.from = sender;
+}
+
+Mailer.prototype.setReceiver = function(receiver) {
+  this.options.to = receiver;
+}
+
+Mailer.prototype.setSubject = function(subject) {
+  this.options.subject = subject;
+}
+
+Mailer.prototype.setHTML = function(html) {
+  this.options.html = html;
+}
+
+Mailer.prototype.setOptions = function(options) {
+  this.options = options;
+}
+
+module.exports = Mailer;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Mailer = require('../mailers/survey_mailer.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,33 +8,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/mailer', function(req, res, next) {
-  var nodemailer = require('nodemailer');
-  
-  var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: "alextowli@gmail.com",
-            pass: "Alex7towli1114"
-        }
-  });
-  
+  console.log("hello world");
   var html = "<h1>Hi Alex</h1>";
   html += "<p><a href=\"http://epro-rest.herokuapp.com/surveys/58d862131d41f7002a6a8891\">Your survey</a></p>";
-
+  var surveyMailer = new Mailer();
   mailOptions = {
      from: 'arnold@schwarzenegger',
      to: 'alextowli@hotmail.co.uk',
      subject: 'Your survey',
      html: html
   };
-
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error)
-      return console.log(error);
-    console.log('Message %s sent: %s', info.messageId, info.response);
-    transporter.close();
-  });
-
+  surveyMailer.setOptions(mailOptions);
+  surveyMailer.sendMail();
+  console.log(surveyMailer);
   res.redirect('/');
 });
 
