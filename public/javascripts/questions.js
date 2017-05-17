cad80b2a7434b5fa422b6e915522dee0b58ed1a7
Alex Towli <alextowli@hotmail.co.uk>
2017-05-17 01:02:27 +0100

    add removing of procedure questions and style

--- public/javascripts/questions.js
+++ public/javascripts/questions.js
@@ -4,10 +4,11 @@ $(document).ready(function(){
   var questionsContainer = $('.question-set-list ul');
   var newQuestionBtn = $('.new-question');
   var addQuestionBtn = $('#question-add');
+  var removeQuestionBtn = $('.question-remove');
   var newQuestionContainer = $('.new-question-container');
- 
-  newQuestionBtn.click(showQuestionSelector);
-  addQuestionBtn.click(addQuestion);
+  var removeButtonHTML = "<button type=\"button\" class=\"btn btn-xs btn-danger question-remove\">Remove</button>";
+
+  setEventHandlers();
 
   function showQuestionSelector(){
     newQuestionContainer.show();
@@ -19,9 +20,27 @@ $(document).ready(function(){
     var questionTitle = selectedQuestion.text();
     var questionID = selectedQuestion.val();
     var hiddenInputField = "<input type=\"hidden\" name=\"question\" value=\""+ questionID +"\">";
-    questionsContainer.append("<li>" + questionTitle + hiddenInputField + "</li>");
+    
+    questionsContainer.append("<li>" + removeButtonHTML + questionTitle + hiddenInputField + "</li>");
     newQuestionContainer.hide();
     newQuestionBtn.show();
+    removeQuestionBtn = $('.question-remove');
+    removeQuestionBtn.click(removeQuestion);
+    findRemoveButtons();
+  }
+
+  function findRemoveButtons() {
+    removeQuestionBtn = $('.question-remove');
+  }
+
+  function setEventHandlers() {
+    newQuestionBtn.click(showQuestionSelector);
+    removeQuestionBtn.click(removeQuestion);
+    addQuestionBtn.click(addQuestion);
+  }
+
+  function removeQuestion() {
+    $(this).parent('li').remove();
   }
 
 });
